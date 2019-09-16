6ee964cc73ba65866d9235b62e25a8427f740300
azwalzaman <azwalzaman5@gmail.com>
2019-09-16 16:17:28 +0530

    program modified to be more efficient with sorting

--- pair_with_given_sum.dart
+++ pair_with_given_sum.dart
@@ -1,19 +1,17 @@
-void findGivenSumInArray(List<int> arr, int sum)
-{
-  for(int i = 0; i < arr.length; i++)
-  {
-    for(int j = i+1; j < arr.length; j++)
-    {
-      if( arr[i] + arr[j] == sum)
-      {
-        print("Sum found at $i and $j ( ${arr[i]} and ${arr[j]} )");
-      }
+void findGivenSumInArray(List<int> arr, int sum) {
+  arr.sort();
+  int low = 0;
+  int high = arr.length - 1;
+
+  while (low < high) {
+    if (arr[low] + arr[high] == sum) {
+      print("Sum found at $low and $high ( ${arr[low]} and ${arr[high]} )");
     }
+    (arr[low] + arr[high] < sum ? low++ : high--);
   }
 }
 
-void main()
-{
-  List<int> list = [1,6,5,4,5];
-  findGivenSumInArray(list, 10);
+void main() {
+  List<int> list = [1, 6, 5, 4, 5];
+  findGivenSumInArray(list, 5);
 }
